84265fda2fc487b3f6c2f790c0d0c3eb3e12f1d7
hsduc <hosiduc2002.chem@gmail.com>
2026-06-15 23:23:05 +0700

    init: claude and gitignore

--- install.sh
+++ install.sh
@@ -41,5 +41,3 @@ mkdir weights
 cd ./weights/
 wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
 wget https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth
-
-
